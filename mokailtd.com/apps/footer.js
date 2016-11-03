8d9e7d63bdb525a03287283aa9a8b09a6b79e77d
Mokai0 <mokai86@gmail.com>
2016-11-03 01:41:01 -0400

    broke links to personal socail media accounts

---
+++ mokailtd.com/apps/footer.js
@@ -0,0 +1,15 @@
+  var twitterHandle = '<a href="construction.html"><img src="img/twitter-wrap.png" alt="Twitter Logo" class="social-icon"></a>'
+  var facebookHandle = '<a href="construction.html"><img src="img/facebook-wrap.png" alt="Facebook Logo" class="social-icon"></a>'
+  var copyright = '<p>&copy; 2016 Mokai.</p>'
+
+var footNote = document.getElementsByTagName("footer");
+
+footNote[0].innerHTML += '<br>'
+
+footNote[0].innerHTML += twitterHandle;
+
+footNote[0].innerHTML += facebookHandle;
+
+footNote[0].innerHTML += '<br>'
+
+footNote[0].innerHTML += copyright;
